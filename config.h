4f6ad47f6be379b00093390d4167784da732e0b0
blascarr <adrian.blascarr@gmail.com>
2016-07-23 10:07:12 +0200

    Added config pins for different boards

---
+++ config.h
@@ -0,0 +1,26 @@
+/*
+  Design and created by Blascarr
+
+  TFTHexel
+  Name    : Blascarr
+  Description: config.h
+  version : 1.0
+
+  PixelArt graphics library for TFT Displays based on Hexels format to draw with several grid frames.
+  The library use different polygons in order to stablish different grids to draw in pixel format:
+
+  Here we can change pins for TFT Connection on different boards.
+
+
+  
+  Blascarr invests time and resources providing this open source code like some other libraries, please
+  respect the job and support open-source hardware.
+  
+  **
+  Written by Adrian for Blascarr
+*/
+
+#define mosi 16
+#define cs   7
+#define rs   0
+#define rst  1
